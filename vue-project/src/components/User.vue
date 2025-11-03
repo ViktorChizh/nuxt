1088dea091d288e38cd530690b44245932cff9e0
ViktorChizh <victor.chizh@mail.ru>
2025-11-03 12:09:07 +0300

    feat: add user component

---
+++ vue-project/src/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+  <div className="user">
+    <button @click="deleteUser(index)">X</button>
+    <h3>{{ user.name }}</h3>
+    <p>{{ user.email }} - <b>{{ user.pass }}</b></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+        user: {
+        type: Object,
+        require: true
+        },
+        index: {
+            type: Number,
+            require: true
+        },
+        deleteUser: {
+            type: Function,
+            require: true
+        }
+    }
+  }
+</script>
+
+<style scoped>
+  button {
+    border: 0;
+    border-radius: 5px;
+    outline: none;
+    padding: 10px;
+    background: #d66c75;
+    color: #7f1629;
+    font-weight: bold;
+    cursor: pointer;
+    transition:  transform 500ms ease;
+    float: right;
+  }
+  button:hover {
+    transform: translateY(-5px);
+  }
+</style>
